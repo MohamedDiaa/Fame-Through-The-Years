f3a786ce99caa069a7e0421914cbe2fc3a9e7c74
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2024-10-10 03:04:50 +0300

    add migrations

--- CelebrityAging/Program.cs
+++ CelebrityAging/Program.cs
@@ -27,6 +27,7 @@ builder.Services.AddAuthentication(options =>
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString)
+
     );
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
